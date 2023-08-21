7297f94e73ad588ffa1f89d44dcb306d2c36adea
Rachit Rastogi <rachit.rastogi31@gmail.com>
2023-08-21 19:38:05 +0530

    add resolver function , datasource and update server.js to able to fetch live data from apollo server

---
+++ odyssey-lift-off-part1/server/datasources/track-api.js
@@ -0,0 +1,19 @@
+const { RestDataSource} = require("@apollo/datasource-rest");
+
+class TrackAPI extends RESTDataSource {
+    baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  
+    getTracksForHome () {
+        return this.get("tracks");
+    }
+
+    getAuthor (authorId) {
+
+        return this.get("/author/${authorId)");
+    }
+
+
+}
+
+
+  module.exports = TrackAPI;
